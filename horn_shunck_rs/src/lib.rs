43eeff425658c0e79629c938bd76a8b6620b86e9
Anas Amrouche <amrouche.an@gmail.com>
2026-04-12 14:26:18 +0200

    Fichier lib peaufiné

--- horn_shunck_rs/src/lib.rs
+++ horn_shunck_rs/src/lib.rs
@@ -8,7 +8,7 @@ type Video = (Py<PyArray3<f64>>, Py<PyArray3<f64>>);
 mod horn_schunck_rs {
     use ndarray::Array2;
     use pyo3::prelude::*;
-    use numpy::{IntoPyArray, PyArray1, PyArray3, PyReadonlyArray3, ToPyArray, ndarray::{Array3, ArrayView2, Axis}};
+    use numpy::{IntoPyArray, PyReadonlyArray3,  ndarray::{Array3, ArrayView2, Axis}};
     
     use crate::{Fields, Video, utilities::{downscale_recursively, expand, get_average, space_derive, time_derive}};
 
@@ -80,8 +80,8 @@ mod horn_schunck_rs {
         )
     }
 
-    fn gradient_descent(image1: ArrayView2<'_, f64>, image2: ArrayView2<'_, f64>, alpha_squared: f64, step: f64, max_iter: u32, tol: f64, normL2: bool) -> (Array2<f64>, Array2<f64>, u32) {
-        if normL2 {
+    fn gradient_descent(image1: ArrayView2<'_, f64>, image2: ArrayView2<'_, f64>, alpha_squared: f64, step: f64, max_iter: u32, norm_l2: bool) -> Fields {
+        if norm_l2 {
             let image_height = image1.shape()[0];
             let image_width = image1.shape()[1];
     
@@ -98,37 +98,19 @@ mod horn_schunck_rs {
                 field[[x_previous, y_index]] + field[[x_next, y_index]] + field[[x_index, y_previous]] + field[[x_index, y_next]]
             };
 
-            let get_gradient_norm = |field: &Array2<f64>, x_index: usize, y_index: usize| -> f64 {
-                let x_previous = x_index.saturating_sub(1).clamp(0, image_height - 1);
-                let x_next = (x_index + 1).min(image_height - 1);
-    
-                let y_previous = y_index.saturating_sub(1).clamp(0, image_width - 1);
-                let y_next = (y_index + 1).min(image_width - 1);
-    
-                (field[[x_next, y_index]] - field[[x_previous, y_index]]).powi(2)/4.0 + (field[[x_index, y_next]] - field[[x_index, y_previous]]).powi(2)/4.0
-            };
-
-            let mut count = 0;
             for _ in 0..max_iter {
-                count += 1;
-                let mut previous_evaluation: f64 = 0.0;
-                let mut next_evaluation: f64 = 0.0;
                 for x_index in 0..image_height {
                     for y_index in 0..image_width {
-                        let (Ix, Iy) = space_derive(image1, x_index, y_index);
-                        let It = time_derive(image1, image2, x_index, y_index);
+                        let (ix, iy) = space_derive(image1, x_index, y_index);
+                        let it = time_derive(image1, image2, x_index, y_index);
                         
-                        next_evaluation += (Ix * u_field[[x_index, y_index]] + Iy * v_field[[x_index, y_index]] + It).powi(2) + get_gradient_norm(&u_field, x_index, y_index) + get_gradient_norm(&v_field, x_index, y_index);
-                        u_field[[x_index, y_index]] -= step * 2.0 * (Ix * (Ix * u_field[[x_index, y_index]] + Iy * v_field[[x_index, y_index]] + It) - alpha_squared * (get_cross_pattern(&u_field, x_index, y_index) - 4.0 * u_field[[x_index, y_index]]));
-                        v_field[[x_index, y_index]] -= step * 2.0 * (Iy * (Ix * u_field[[x_index, y_index]] + Iy * v_field[[x_index, y_index]] + It) - alpha_squared * (get_cross_pattern(&v_field, x_index, y_index) - 4.0 * v_field[[x_index, y_index]]));
+                        u_field[[x_index, y_index]] -= step * 2.0 * (ix * (ix * u_field[[x_index, y_index]] + iy * v_field[[x_index, y_index]] + it) - alpha_squared * (get_cross_pattern(&u_field, x_index, y_index) - 4.0 * u_field[[x_index, y_index]]));
+                        v_field[[x_index, y_index]] -= step * 2.0 * (iy * (ix * u_field[[x_index, y_index]] + iy * v_field[[x_index, y_index]] + it) - alpha_squared * (get_cross_pattern(&v_field, x_index, y_index) - 4.0 * v_field[[x_index, y_index]]));
                     }
                 }
-                if (next_evaluation - previous_evaluation).abs() < tol {break};
-                previous_evaluation = next_evaluation;
-                next_evaluation = 0.0;
             }
     
-            (u_field, v_field, count)
+            (u_field, v_field,)
         }
         else {
             let image_height = image1.shape()[0];
@@ -147,21 +129,19 @@ mod horn_schunck_rs {
                 (field[[x_next, y_index]] - field[[x_index, y_index]]).signum() + (field[[x_index, y_index]] - field[[x_previous, y_index]]).signum() + (field[[x_index, y_next]] - field[[x_index, y_index]]).signum() + (field[[x_index, y_index]] - field[[x_index, y_previous]]).signum()
             };
 
-            let mut count = 0;
             for _ in 0..max_iter {
-                count += 1;
                 for x_index in 0..image_height {
                     for y_index in 0..image_width {
-                        let (Ix, Iy) = space_derive(image1, x_index, y_index);
-                        let It = time_derive(image1, image2, x_index, y_index);
+                        let (ix, iy) = space_derive(image1, x_index, y_index);
+                        let it = time_derive(image1, image2, x_index, y_index);
     
-                        u_field[[x_index, y_index]] -= step * 2.0 * (Ix * (Ix * u_field[[x_index, y_index]] + Iy * v_field[[x_index, y_index]] + It) - alpha_squared * (get_cross_pattern(&u_field, x_index, y_index) - 4.0 * u_field[[x_index, y_index]]));
-                        v_field[[x_index, y_index]] -= step * 2.0 * (Iy * (Ix * u_field[[x_index, y_index]] + Iy * v_field[[x_index, y_index]] + It) - alpha_squared * (get_cross_pattern(&v_field, x_index, y_index) - 4.0 * v_field[[x_index, y_index]]));
+                        u_field[[x_index, y_index]] -= step * 2.0 * (ix * (ix * u_field[[x_index, y_index]] + iy * v_field[[x_index, y_index]] + it) - alpha_squared * (get_cross_pattern(&u_field, x_index, y_index) - 4.0 * u_field[[x_index, y_index]]));
+                        v_field[[x_index, y_index]] -= step * 2.0 * (iy * (ix * u_field[[x_index, y_index]] + iy * v_field[[x_index, y_index]] + it) - alpha_squared * (get_cross_pattern(&v_field, x_index, y_index) - 4.0 * v_field[[x_index, y_index]]));
                     }
                 }
             }
 
-            (u_field, v_field, count)
+            (u_field, v_field)
         }
     }
 
@@ -172,10 +152,9 @@ mod horn_schunck_rs {
             alpha_squared: f64,
             step: f64,
             max_iter: u32,
-            tol: f64,
-            normL2: bool
+            norm_l2: bool
         )
-        -> (Py<PyArray3<f64>>, Py<PyArray3<f64>>, Py<PyArray1<u32>>) {
+        -> Video  {
             let video_array = video.as_array();
 
             let (frame_count, frame_height, frame_width) = (video_array.shape()[0], video_array.shape()[1], video_array.shape()[2]);
@@ -183,27 +162,25 @@ mod horn_schunck_rs {
             let mut u_field = Array3::<f64>::zeros((frame_count, frame_height, frame_width));
             let mut v_field = Array3::<f64>::zeros((frame_count, frame_height, frame_width));
 
-            let mut counts: Vec<u32> = Vec::new();
             for k in 0..frame_count-1 {
                 let current_frame = video_array.index_axis(Axis(0), k);
                 let next_frame = video_array.index_axis(Axis(0), k+1);
 
-                let (u, v, count) = gradient_descent(current_frame, next_frame, alpha_squared, step, max_iter, tol, normL2);
+                let (u, v) = gradient_descent(current_frame, next_frame, alpha_squared, step, max_iter, norm_l2);
 
                 u_field.index_axis_mut(Axis(0), k).assign(&u);
                 v_field.index_axis_mut(Axis(0), k).assign(&v);
-
-                counts.push(count);
             }
 
             (
                 u_field.into_pyarray(py).unbind(),
                 v_field.into_pyarray(py).unbind(),
-                counts.to_pyarray(py).unbind()
             )
         }
 
     fn gauss_seidel_from_previous_uv(image1: ArrayView2<'_, f64>, image2: ArrayView2<'_, f64>, prev_u_field: Array2<f64>, prev_v_field: Array2<f64>, alpha_squared: f64, iter_max: u32) -> Fields {
+        // Cette fonction sert à généraliser Gauss-Seidel dans les cas où un champ précédent a été calculé de façon pyramidale
+        // Note pour plus tard : Je devrais peut-être l'utiliser à la place de la première méthode que j'ai mise en place car celle-ci est plus générale et un seule fonction rendrait le programme plus lisible.
         let image_height = image1.shape()[0];
         let image_width = image1.shape()[1];
 
@@ -241,6 +218,7 @@ mod horn_schunck_rs {
     }
 
     fn pyramid(recursion_depth: u8, image1: ArrayView2<'_, f64>, image2: ArrayView2<'_, f64>, alpha_squared: f64, iter_max: u32) -> Fields {
+        // Au cas où on irait trop loin avec les récursions au point où l'image devient trop petite pour le noyau gaussien
         if recursion_depth == 0 {
             return gauss_seidel(image1, image2, alpha_squared, iter_max);
         }
@@ -248,19 +226,16 @@ mod horn_schunck_rs {
         let downscaled_images_1 = downscale_recursively(image1, recursion_depth);
         let downscaled_images_2 = downscale_recursively(image2, recursion_depth);
 
-        // On commence par l'image la plus petite (l'indice le plus élevé du tableau)
-        let coarsest_idx = (recursion_depth - 1) as usize;
+        let most_downscaled_idx = (recursion_depth - 1) as usize;
         
-        // On initialise u et v en mutable pour éviter le shadowing dans la boucle
         let (mut u, mut v) = gauss_seidel(
-            downscaled_images_1[coarsest_idx].view(), 
-            downscaled_images_2[coarsest_idx].view(), 
+            downscaled_images_1[most_downscaled_idx].view(), 
+            downscaled_images_2[most_downscaled_idx].view(), 
             alpha_squared, 
             iter_max
         );
 
-        // On remonte la pyramide à l'envers, du niveau N-1 jusqu'au niveau 0
-        for k in (0..coarsest_idx).rev() {
+        for k in (0..most_downscaled_idx).rev() {
             let current_image_1 = downscaled_images_1[k].view();
             let current_image_2 = downscaled_images_2[k].view();
             
@@ -279,7 +254,6 @@ mod horn_schunck_rs {
             v = new_v;
         }
 
-        // Étape finale absolue : on affine sur les images d'origine en taille réelle !
         let target_shape = (image1.shape()[0], image1.shape()[1]);
         let expanded_u = expand(u.view(), target_shape.0, target_shape.1);
         let expanded_v = expand(v.view(), target_shape.0, target_shape.1);
@@ -330,7 +304,7 @@ mod utilities {
         let image_width = image.shape()[1];
 
         //Remplacement du bloc conditionnel par du clamping. Ça devrait permettre au compilateur d'appliquer des optimisations
-        //Impossibles à mettre en place avex les blocs match (SIMD)
+        //Impossibles à mettre en place avex les blocs match (SIMD par exemple)
         let x_previous = x.saturating_sub(1);
         let x_next = (x + 1).min(image_height - 1);
         
@@ -382,11 +356,10 @@ mod utilities {
         image[[x_index+1, y_index+1]] + image[[x_index-1, y_index+1]] + image[[x_index+1, y_index-1]] + image[[x_index-1, y_index-1]]
     }
 
-    pub fn expand_f64(upscaled_image: &mut Array2<f64>, value: f64, x_center: usize, y_center: usize) {
+    pub fn expand_pixel(upscaled_image: &mut Array2<f64>, value: f64, x_center: usize, y_center: usize) {
         let h = upscaled_image.shape()[0];
         let w = upscaled_image.shape()[1];
         
-        // Remplacement par des itérations bornées (plus sûr et gère les bords cibles)
         for dx in 0..=2 {
             for dy in 0..=2 {
                 let x = x_center.saturating_add(dx).saturating_sub(1);
@@ -431,42 +404,10 @@ mod utilities {
             for y_index in 0..downscaled_width {
                 let (x_expanded_index, y_expanded_index) = (x_index*3 + 1, y_index*3 + 1);
                 
-                // Rigueur : on multiplie par 3 car l'échelle est 3 fois plus grande
                 let scaled_value = downscaled_image[[x_index, y_index]] * 3.0;
-                expand_f64(&mut expanded_image, scaled_value, x_expanded_index, y_expanded_index);
+                expand_pixel(&mut expanded_image, scaled_value, x_expanded_index, y_expanded_index);
             }
         }
         expanded_image
     }
-}
-
-#[cfg(test)]
-mod tests {
-    use ndarray::{Array2};
-    use numpy::array;
-    use crate::utilities::{downscale, expand};
-
-    #[test]
-    fn simple_downscale() {
-        let to_downscale: Array2<f64> = array![[2.0, 2.0, 2.0, 1.0, 1.0, 1.0], [2.0, 2.0, 2.0, 1.0, 1.0, 1.0], [2.0, 2.0, 2.0, 1.0, 1.0, 1.0], [1.0, 1.0, 1.0, 3.0, 3.0, 3.0], [1.0, 1.0, 1.0, 3.0, 3.0, 3.0], [1.0, 1.0, 1.0, 3.0, 3.0, 3.0]];
-        let downscaled = downscale(to_downscale.view());
-
-        println!("Downscaled values: {:?}", downscaled);
-    }
-
-    // #[test]
-    // fn simple_expand() {
-    //     let to_expand = array![[2.0, 1.0], [1.0, 3.0]];
-    //     let expanded = expand(to_expand.view());
-
-    //     println!("Expanded values{:#?}", expanded);
-    // }
-
-    // #[test]
-    // fn downscale_expand() {
-    //     let to_transform: Array2<f64> = array![[2.0, 2.0, 2.0, 1.0, 1.0, 1.0], [2.0, 2.0, 2.0, 1.0, 1.0, 1.0], [2.0, 2.0, 2.0, 1.0, 1.0, 1.0], [1.0, 1.0, 1.0, 3.0, 3.0, 3.0], [1.0, 1.0, 1.0, 3.0, 3.0, 3.0], [1.0, 1.0, 1.0, 3.0, 3.0, 3.0]];
-    //     let transformed = expand(downscale(to_transform.view()).view());
-
-    //     println!("Composed array: {:#?}", transformed);
-    // }
 }
